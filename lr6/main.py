860f57a526f0ad198adfdea12a090f58a8df5c55
Galya <gufkz.tkrbyf@yandex.ru>
2020-05-21 17:04:14 +0300

    форма v1.0

--- lr6/main.py
+++ lr6/main.py
@@ -2,7 +2,7 @@ import re
 
 countryList = ['Russia','Finland','Britain','France','Germany','Spain','Italy']
 
-class NewRegistration():
+class NewRegistration:
     def __init__(self, fname, lname, email, affil, year, country, city, postalCode, adress, hdyk):
         try:
             arg1 = self.__fname_check(fname)
@@ -31,87 +31,81 @@ class NewRegistration():
             print('Ваша заявка принята')
 
     def __fname_check(self, value):
+        if value == '':
+            raise ValueError('Заполните параметр First Name')
         if len(value) < 2:
             raise ValueError('Параметр First Name должен содержать не менее 2 символов')
-        if re.search('[0-9]', value) != None:
+        if re.search(r'[0-9]', value) != None:
             raise ValueError('Параметр First Name не должен содержать цифр')
         return value
     
     def __lname_check(self, value):
-        if re.search('[0-9]', value) != None:
+        if value == '':
+            raise ValueError('Заполните параметр Last Name')
+        if re.search(r'[0-9]', value) != None:
             raise ValueError('Параметр Last Name не должен содержать цифр')
         return value
     
     def __email_check(self, value):
-        if re.fullmatch(r'[\w.-]+@[\w.-]+\.?[\w]+?',value) == False:
+        if value == '':
+            raise ValueError('Заполните параметр Email')
+        if re.fullmatch(r'.+?@.+?\..{1,4}',value) == None:
             raise ValueError('Неверно введен параметр Email')
         return value
             
     def __affil_check(self, value):
+        if value == '':
+            raise ValueError('Заполните параметр Affiliation')
+        if re.search(r'[0-9]', value) != None:
+            raise ValueError('Параметр Affiliation не должен содержать цифр')
         if value.find('University') == -1:
             raise ValueError('Параметр Affiliation должен содержать слово "University"')
         return value
     
     def __year_check(self, value):
-        if int(value) < 1900 and int(value) > 2015:
+        if value == '':
+            raise ValueError('Заполните параметр Year of birth')
+        if int(value) < 1900 or int(value) > 2015:
             raise ValueError('Невозможное значение параметра Year of birth')
         return value
     
     def __country_check(self, value):
+        if value == '':
+            raise ValueError('Заполните параметр Country')
         if value not in countryList:
-            raise ValueError('Данная страна не принимает участие в конференции')
+            raise ValueError('Неверное заполнение параментра Country. Данная страна не принимает участие в конференции')
         return value
     
     def __city_check(self, value):
-        if re.search('[0-9]', value) != None:
+        if value == '':
+            raise ValueError('Заполните параметр City')
+        if re.search(r'[0-9]', value) != None:
             raise ValueError('Параметр City не должен содержать цифр')
         if len(value) < 4:
             raise ValueError('Параметр City должен содержать не менее 4 символов')
         return value
     
     def __postalCode_check(self, value):
+        if value == '':
+            raise ValueError('Заполните параметр Postal Code')
         if len(value) < 6:
             raise ValueError('Параметр Postal Code должен содержать не менее 6 цифр')
-        if re.search('\D',value) != None:
+        if re.search(r'\D',value) != None:
             raise ValueError('Параметр Postal Code должен состоять только из цифр')
         return value
     
     def __adress_check(self, value):
-        re.fullmatch('st.[\w]+?, [\d]+?', value) == False:
+        if value == '':
+            raise ValueError('Заполните параметр Adress')
+        if re.fullmatch(r'st\.[\s]?[\w]+?,[\s]?[\d]+?[\w]*?', value) == None:
             raise ValueError('Неверно введен параметр Adress. Введите его по шаблону "st. NameOfStreet, NumberOfHouse"')
         return value
     
     def __hdyk_check(self, value):
         if value == '':
             raise ValueError('Заполните параметр "How Did You Know About Us?"')
+        if len(value) < 3:
+            raise ValueError('Параметр "How Did You Know About Us?" заполнен не корректно')
         return value
 
-new_form = NewRegistration('Anna','Piterson','fuflya12@yahoo.com','Herzen University','1999', 'Russia', 'St.Petersburg', '196521', 'st. Vishnevskogo, 52', 'Internet')
-# https://trinket.io/python3/ed41c9c244
-# https://trinket.io/python3/fbd996a9df
-
-
-# class User():
-#   def __init__(self):
-#     self.__email = ""
-#     self.__password = ""
-#     
-#   @property
-#   def email(self):
-#     return self.__email
-#     
-#     
-#   @email.setter
-#   def email(self, new_email):
-#     # проверка email на валидность
-#     self._email = new_email
-# 
-# 
-#   def __del__(self):
-#     print('Deleting object ... done')
-# 
-# # u1 = User()
-# # print('other way to delete object')
-# # u1 = '' 
-# 
-# # del u1
+new_form = NewRegistration('Ann','Li','say@google.com','Herzen University', '1999', 'Russia', 'St.Petersburg', '123923', 'st.Vishnevskogo,56', 'Internet')

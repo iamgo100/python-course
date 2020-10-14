1372cafc1f961cdbc60ad4c39f191f7cb12c74f9
Галя Елкина <gufkz.tkrbyf@yandex.ru>
2020-10-14 14:59:23 +0300

    Update iterchat

--- Sem5/iterchat/main.py
+++ Sem5/iterchat/main.py
@@ -3,7 +3,8 @@ import tornado.web as web
 import tornado.websocket
 import asyncio
 import json
-from random import randint, choice
+from time import sleep
+from random import choice
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
@@ -11,10 +12,7 @@ answers = ['Hi', 'how RU', 'miss you', "i'm bored", 'you', 'love', 'I', 'I <3 U'
 seq_names = []
 seq_messages = []
 
-def render_list(message):
-    data = json.loads(message)
-    seq_names.append(data['name'])
-    seq_messages.append(data['message'])
+def render_list():
     seq_render = []
     if seq_names:
         for i in range(len(seq_names)):
@@ -26,16 +24,33 @@ class MainHandler(web.RequestHandler):
         self.render('index.html')
 
 class EchoWebSocketHandler(tornado.websocket.WebSocketHandler):
+    def user_message(self, message):
+        data = json.loads(message)
+        seq_names.append(data['name'])
+        seq_messages.append(data['message'])
+        lst = render_list()
+        self.write_message(lst)
+
+    def bots_answer(self):
+        answer = choice(answers)
+        seq_names.append('Chat Bot')
+        seq_messages.append(answer)
+        lst = render_list()
+        self.write_message(lst)
+
+    def chating(self, message):
+        self.user_message(message)
+        sleep(1)
+        self.bots_answer()
+
     def open(self):
         print('websocket is opened')
 
     def on_message(self, message):
-        lst = render_list(message)
-        self.write_message(lst)
+        self.chating(message)
 
     def on_close(self):
         print('websocket is closed')
-        tornado.ioloop.IOLoop.current().stop()
 
 def make_app():
     return web.Application([

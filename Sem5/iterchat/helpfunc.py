fa9786795e2ce7a9b32a655cb26a3891c99e097b
iamgo100 <gufkz.tkrbyf@yandex.ru>
2020-11-18 10:58:57 +0300

    update iterchat

--- Sem5/iterchat/helpfunc.py
+++ Sem5/iterchat/helpfunc.py
@@ -1,6 +1,6 @@
-def render_list(seq_names, seq_messages):
+def render_list(seq):
     seq_render = []
-    if seq_names:
-        for i in range(len(seq_names)):
-            seq_render.append(f'<li><span class="name">{seq_names[i]}</span><span class="message">{seq_messages[i]}</span></li>')
+    if seq:
+        for i in range(len(seq)):
+            seq_render.append(f'<li><span class="name">{seq[i]["name"]}</span><span class="message">{seq[i]["message"]}</span></li>')
     return ''.join(seq_render)
--- Sem5/iterchat/main.py
+++ Sem5/iterchat/main.py
@@ -1,63 +1,70 @@
-import buffer
 import helpfunc as hf
 import tornado.ioloop
 import tornado.web as web
 import tornado.websocket
 import asyncio
 import json
+from tornado.locks import Condition
 from time import sleep
 from random import choice
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-answers = ['Hi', 'how RU', 'miss you', "i'm bored", 'you', 'love', 'I', 'I <3 U', 'RU joking me?', 'sh*t', 'F...!']
-seq_names = []
-seq_messages = []
+def singleton(cls):
+    instances = {}
+    def wrapper(*args, **kwargs):
+        if cls not in instances:
+          instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return wrapper
 
-class Buffer():
+@singleton
+class Buffer:
     def __init__(self):
-        buffer.create_tables()
+        self.__name = ''
+        self.__messages = []
+        self.__answers = ['Hi', 'how RU', 'miss you', "i'm bored", 'you', 'love', 'I', 'I <3 U', 'RU joking me?', 'sh*t', 'F...!']
+
+        self.condition = Condition()
+
+    def set_name(self, name):
+        self.__name = name
+    
+    def get_name(self):
+        return self.__name
+
+    def add_message(self, name, message):
+        self.__messages.append({"name": name, "message": message})
+        self.condition.notify_all()
+    
+    def get_answer(self):
+        return choice(self.__answers)
+
+    def render(self):
+        return hf.render_list(self.__messages)
+    
+    def get_users_messages(self, cursor):
+
+        return hf.render_list("sth")
 
 class StartHandler(web.RequestHandler):
     def get(self):
         self.render('autorization.html')
 
-class AutorizationHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print('autorization is opened')  
-
-    def on_message(self, message):
-        data = json.loads(message)
-        name = data["name"]
-        WebSocketHandler.name = name
-
-    def on_close(self):
-        print('autorization is closed')
-
-# class ChatListHandler(web.RequestHandler):
-#     def get(self):
-#         self.render('chats.html')
-
-class MainHandler(web.RequestHandler):
+class IndexHandler(web.RequestHandler):
     def get(self):
         self.render('index.html')
 
-class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    name = ''
+buffer = Buffer()
 
-    def user_message(self, message):
-        data = json.loads(message)
-        seq_names.append(data['name'])
-        seq_messages.append(data['message'])
-        lst = hf.render_list(seq_names, seq_messages)
-        self.write_message(lst)
+class MainHandler(tornado.websocket.WebSocketHandler):
+    def user_message(self, data):
+        buffer.add_message(buffer.get_name(), data['message'])
+        self.write_message(buffer.render())
 
     def bots_answer(self):
-        answer = choice(answers)
-        seq_names.append('Chat Bot')
-        seq_messages.append(answer)
-        lst = hf.render_list(seq_names, seq_messages)
-        self.write_message(lst)
+        buffer.add_message('Chat Bot', buffer.get_answer())
+        self.write_message(buffer.render())
 
     def chating(self, message):
         self.user_message(message)
@@ -65,23 +72,24 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         self.bots_answer()
 
     def open(self):
-        print('start chating')
-        lst = hf.render_list(seq_names, seq_messages)
-        self.write_message(lst)
+        print('websocket is open')
+        self.write_message(buffer.render())
 
     def on_message(self, message):
-        self.chating(message)
+        data = json.loads(message)
+        if data.get("message"):
+            self.chating(data)
+        else:
+            buffer.set_name(data["name"])
 
     def on_close(self):
-        print('end chating')
+        print('websocket is close')
 
 def make_app():
     return web.Application([
         (r"/", StartHandler),
-        # (r"/chats", ChatListHandler),
-        (r"/index", MainHandler),
-        (r"/websocket", WebSocketHandler),
-        (r"/autorization", AutorizationHandler)
+        (r"/index", IndexHandler),
+        (r"/websocket", MainHandler),
     ], debug=True)
 
 if __name__ == "__main__":

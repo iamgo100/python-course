76e472196d471cb7c17fb9913c0d7e64734dc71b
iamgo100 <gufkz.tkrbyf@yandex.ru>
2020-11-24 09:50:47 +0300

    update Sem5

---
+++ Sem5/patterns/builder.py
@@ -0,0 +1,14 @@
+"""
+    Пример решения задачи создания теста
+"""
+
+class Builder:
+
+    # def new_product(name):
+    #     if name == "Simple car":
+    #         pass
+    #     if name == "Family car":
+    #         pass
+    #     if name == "Sport car":
+    #         pass
+    pass
